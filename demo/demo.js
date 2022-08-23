862ae1050284a8c02361590b07c3ae4f96fd04e9
Chrrrisss <chrisayodele10@gmail.com>
2022-08-23 14:49:58 +0100

    add custom bar

--- demo/demo.js
+++ demo/demo.js
@@ -2,4 +2,18 @@ const range_slider = require("..")
 
 const range = range_slider()
 
-document.body.append(range)
+document.body.innerHTML = `<h1> range slider </h1>`
+
+const main = document.createElement("div")
+main.classList.add("type1")
+
+const style = document.createElement("style")
+style.textContent = `
+    .type1 {
+        border: 1px solid grey;
+        padding: 50px;
+}
+`
+
+main.append(range)
+document.body.append(main, style)
